ac3fe71efbe42ffff6688cb36aecb66a67f663d0
AayushiYUvasoft <Aayushis@yuvasoftech.com>
2024-05-16 03:04:37 -0700

    added-header

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <ul><li>Home</li>
+        <li>Services</li>
+        <li>main</li>
+        <li>code</li><li>hello</li></ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
